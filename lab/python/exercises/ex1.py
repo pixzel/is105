aae72e9eac7f95ca5914f79dc04f4b25ed63d7bf
Ullsokk1 <Sondre.lind@gmail.com>
2014-01-24 18:28:31 +0100

    Lab1
    Exercises 1-9 python

---
+++ lab/python/exercises/ex1.py
@@ -0,0 +1,8 @@
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch touch this.'
+print "Hei, dette er min egen linje"
---
+++ lab/python/exercises/ex3.py
@@ -0,0 +1,28 @@
+#printer ut tekst med: print
+print "I will now count my chickens:"
+
+#her bruker vi plus og slash
+print "hens", 25+30/6
+#Bruker minus, asterisk og percent
+print "Roosters", 100-25*3%4
+
+print "Now I will count the eggs:"
+
+print 3+2+1-5+4%2-1/4+6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+#bruker plus, less-than og minus
+print 3+2<5-7
+
+print "what is 3 + 2?", 3+2
+print "what is 5-7?", 5-7
+
+print "Oh, that's why it's false."
+
+print "How about some more."
+
+#Bruker less-than-equal, greater-than-equal.
+print "Is it greater?", 5>-2
+print "Is it greater or equal?", 5>= -2
+print "Is it less or equal?", 5 <= -2
